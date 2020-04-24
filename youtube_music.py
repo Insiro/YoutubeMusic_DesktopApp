2d634a8cd27dbe22833dc66c80ecfac33f70d34d
PieSignal <leeon@insiro.me>
2020-04-24 23:56:56 +0900

    .

--- youtube_music.py
+++ youtube_music.py
@@ -1,29 +1,33 @@
 import sys
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, Qt
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QColor, QPalette, QIcon
 
 
 class MyApp(QtWidgets.QWidget):
+    titleName: str = "Youtube Music"
+    webURL: str = "https://music.youtube.com"
 
     def __init__(self):
         super().__init__()
         self.initUI()
-        
 
     def initUI(self):
+        self.setWindowIcon(QIcon("./icon.png"))
+        self.setStyleSheet("background:black")
         hbox = QtWidgets.QHBoxLayout()
-        
-        self.setWindowTitle("Youtube Music")
-        web = QWebEngineView()
-        web.setUrl(QUrl("https://music.youtube.com"))
-        
-        hbox.addWidget(web)
+        self.web = QWebEngineView()
+        self.web.back
+        self.setWindowTitle(self.titleName)
+        self.web.setUrl(QUrl(self.webURL))
+
+        hbox.addWidget(self.web)
         self.setLayout(hbox)
         self.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
     ex = MyApp()
